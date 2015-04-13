9dd7c473975a0ccb417ffbc6c0bde573137b8ca0
lomby92 <giacomo.lombardi@live.it>
2015-04-13 10:39:00 +0200

    added get destination

---
+++ src/java/ViewTier/GetDestinations.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewTier;
+
+import BusinessLogicTier.BusinessLogic;
+import BusinessLogicTier.BusinessLogicInterface;
+import DataAccessTier.Destination;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Giacomo
+ */
+public class GetDestinations extends HttpServlet {
+
+    BusinessLogicInterface bl;
+    
+    public void doGet(HttpServletRequest request,
+                  HttpServletResponse response)
+          throws ServletException, IOException{
+        bl = new BusinessLogic();
+        LinkedList<Destination> destinationList;
+        String separator = ";";
+        PrintWriter out = response.getWriter();
+        destinationList = bl.getDestinations(request.getParameter("user"));
+        for(int i=0;i<destinationList.size();i++){
+            out.write(destinationList.get(i).getDevice()+separator);
+        };
+    }
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request,
+                   HttpServletResponse response)
+    throws ServletException, IOException {
+    doGet(request, response);
+    }
+
+
+}
